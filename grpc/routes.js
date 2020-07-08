bf02e0e2faf4d48acb5eff013e1bbc0f372d821c
pierguinzani <49373874+pierguinzani@users.noreply.github.com>
2020-07-08 15:45:20 -0300

    Update routes.js

--- grpc/routes.js
+++ grpc/routes.js
@@ -58,8 +58,7 @@ app.delete("/deleteItem/:id",(req,res)=>{
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Server running at port %d", PORT);
 });
-
